0a484af0f4ff3c4529fcab39634e195d3e95c766
Valeriy Nikolenko <v.nikolenko@corp.mail.ru>
2022-03-24 21:25:36 +0300

    day 5. part 1

--- task5/day5part1.py
+++ task5/day5part1.py
@@ -5,12 +5,17 @@ with open('day5input', 'r') as file:
 
 
 def parse_string(line):
-    mod_line = [0, 1, 2, 3]
+    mod_line = [0, 1, 0, 3]
     return mod_line
 
 
 def write_data(board, mod_string):
-    pass
+    if mod_string[0] == mod_string[2]:
+        for i in range(mod_string[1], mod_string[3]+1):
+            board[i][mod_string[0]] = board[i][mod_string[0]] + 1
+    else:
+        for i in range(mod_string[0], mod_string[2]+1):
+            board[mod_string[1]][i] = board[mod_string[1]][i] + 1
 
 
 def count_result(board):
@@ -25,9 +30,9 @@ for i in range(0, size):
         subboard.append(0)
     board.append(subboard)
 
+write_data(board, [0, 1, 4, 1])
+# for i in range(0, len(input_lines)):
+#     mod_string = parse_string(input_lines[i])
+#     write_data(board, mod_string)
+#
 print(board)
-for i in range(0, len(input_lines)):
-    mod_string = parse_string(input_lines[i])
-    write_data(board, mod_string)
-
-print(count_result(board))
