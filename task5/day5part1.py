cf7d4dfe3c573d04336942dc7d1fbbb025896b97
Valeriy Nikolenko <v.nikolenko@corp.mail.ru>
2022-03-26 19:49:43 +0300

    fix

--- task5/day5part1.py
+++ task5/day5part1.py
@@ -8,7 +8,7 @@ def parse_string(mod_line):
     mod_line = mod_line.split(" -> ")
     mod_line[0] = mod_line[0].split(",")
     mod_line[1] = mod_line[1].split(",")
-    mod_line = [mod_line[0][0], mod_line[0][1], mod_line[1][0], mod_line[1][1]]
+    mod_line = [int(mod_line[0][0]), int(mod_line[0][1]), int(mod_line[1][0]), int(mod_line[1][1])]
     return mod_line
 
 
@@ -16,7 +16,7 @@ def write_data(board, mod_string):
     if mod_string[0] == mod_string[2]:
         for i in range(min(mod_string[1], mod_string[3]), max(mod_string[1], mod_string[3])+1):
             board[i][mod_string[0]] = board[i][mod_string[0]] + 1
-    else:
+    if mod_string[1] == mod_string[3]:
         for i in range(min(mod_string[0], mod_string[2]), max(mod_string[0], mod_string[2])+1):
             board[mod_string[1]][i] = board[mod_string[1]][i] + 1
 
@@ -31,17 +31,15 @@ def count_result(board):
 
 
 board = []
-size = 10
+size = 1000
 for i in range(0, size):
     subboard = []
     for j in range(0, size):
         subboard.append(0)
     board.append(subboard)
 
-write_data(board, [9, 8, 9, 2])
-print(board)
-# for i in range(0, len(input_lines)):
-#     mod_string = parse_string(input_lines[i])
-#     write_data(board, mod_string)
-#
-# print(parse_string(input_lines[2]))
+for i in range(0, len(input_lines)):
+    mod_string = parse_string(input_lines[i])
+    write_data(board, mod_string)
+
+print(count_result(board))
