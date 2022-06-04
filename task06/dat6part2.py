d9d8ff84af03abfd011b81270ffcfac362cbfec3
bender <val.vrn@gmail.com>
2022-06-04 20:09:58 +0300

    add fix version for 256 days

--- task06/dat6part2.py
+++ task06/dat6part2.py
@@ -8,20 +8,37 @@ def fish_count(born, end_day):
     old_fish = 6
     new_fish = 8
     fish_list = [born]
-    # TODO: Переделать тут!
-    new_fish_list = {born: 1}
     for i in range (1, end_day + 1):
         for j in range (0, len(fish_list)):
             fish_list[j] = fish_list[j] - 1
             if fish_list[j] == -1:
                 fish_list[j] = old_fish
                 fish_list.append(new_fish)
-        print(f'{i}: {len(fish_list)}')
+        print(f'Day {i} array {len(fish_list)}')
     return len(fish_list)
+
+def fish_count_fix(born, max_day):
+    fish_dict_template = {8: 0, 7: 0, 6: 0, 5: 0, 4: 0, 3: 0, 2: 0, 1: 0, 0: 0}
+    fish_dict_current = fish_dict_template.copy()
+    fish_dict_new = fish_dict_current.copy()
+
+    fish_dict_current[born] = 1
+    for i in range (1, max_day+1):
+        for j in range (0, 9):
+            if j >= 1:
+                fish_dict_new[j - 1] = fish_dict_current[j] + fish_dict_new[j - 1]
+            else:
+                fish_dict_new[6] = fish_dict_current[0]
+                fish_dict_new[8] = fish_dict_current[0]
+                fish_dict_current[0] = 0
+        fish_dict_current = fish_dict_new
+        fish_dict_new = fish_dict_template.copy()
+    return sum(fish_dict_current.values())
+
 
 result = 0
 for i in range (1, 6):
-    result = result + (fish_start.count(i) * fish_count(i, max_day))
-
+    result = result + (fish_start.count(i) * fish_count_fix(i, max_day))
 print(result)
+
 
